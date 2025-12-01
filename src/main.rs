3eff0ea9d4cad7893e7ffd8db71291091b169e1e
matee8 <graves-bluff-pesky@duck.com>
2025-12-01 23:13:20 +0100

    feat(main): implement CLI for running solutions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let day = args.get(1).and_then(|day| day.parse().ok()).unwrap_or(1);
+
+    match day {
+        _ => eprintln!("Day {day} not implemented yet."),
+    }
 }
