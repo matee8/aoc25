b902f47dfbfaa94a1236d648e4921379bd034097
matee8 <graves-bluff-pesky@duck.com>
2025-12-03 15:56:27 +0100

    feat(day02): implement day 2 part 2 solution

--- src/day02.rs
+++ src/day02.rs
@@ -28,13 +28,21 @@ impl Display for InputError {
 pub fn main() {
     let input = include_str!("../assets/02.txt");
 
-    match solve_part1(input) {
+    match solve_task(input, solve_part1) {
         Ok(value) => println!("Solution for part 1: {value}"),
+        Err(e) => eprintln!("Error: {e}"),
+    }
+
+    match solve_task(input, solve_part2) {
+        Ok(value) => println!("Solution for part 2: {value}"),
         Err(e) => eprintln!("Error: {e}"),
     }
 }
 
-fn solve_part1(input: &str) -> Result<i128, InputError> {
+fn solve_task<F>(input: &str, solver: F) -> Result<i128, InputError>
+where
+    F: Fn(i128, i128) -> i128,
+{
     let mut invalid_ids = 0;
 
     for id_range in input.split(',') {
@@ -68,18 +76,50 @@ fn solve_part1(input: &str) -> Result<i128, InputError> {
             return Err(InputError::Number);
         };
 
-        for id in first..=last {
-            let id_str = id.to_string();
+        invalid_ids += solver(first, last);
+    }
 
-            if id_str.len() % 2 != 0 {
+    Ok(invalid_ids)
+}
+
+fn solve_part1(first: i128, last: i128) -> i128 {
+    let mut invalid_ids = 0;
+
+    for id in first..=last {
+        let id_str = id.to_string();
+
+        if id_str.len() % 2 != 0 {
+            continue;
+        }
+
+        if id_str[..id_str.len() / 2] == id_str[id_str.len() / 2..] {
+            invalid_ids += id;
+        }
+    }
+
+    invalid_ids
+}
+
+fn solve_part2(first: i128, last: i128) -> i128 {
+    let mut invalid_ids = 0;
+
+    for id in first..=last {
+        let id_str = id.to_string();
+
+        for n in 1..=id_str.len() / 2 {
+            if id_str.len() % n != 0 {
                 continue;
             }
 
-            if id_str[..id_str.len() / 2] == id_str[id_str.len() / 2..] {
+            let sequence = &id_str[0..n];
+            let repetitions = id_str.len() / n;
+
+            if sequence.repeat(repetitions) == id_str {
                 invalid_ids += id;
+                break;
             }
         }
     }
 
-    Ok(invalid_ids)
+    invalid_ids
 }
